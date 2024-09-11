ed6bd51f5ce8e1a784cc4bb9b4511ab55e541213
shuminchang <magic2313@gmail.com>
2024-09-11 09:31:31 +0800

    add analysis app

--- job104_spider/analysis/views.py
+++ job104_spider/analysis/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .job104_spider import Job104Spider
 
 # Create your views here.
+def index(request):
+    total_count = request.session.get('total_count', 0)
+    keyword = request.session.get('keyword', '')
+
+    return render(request, 'analysis/index.html', {
+        'total_count': total_count,
+        'keyword': keyword
+    })
